4f015d098c5a4cbc42baf849136e231871fdbaca
Pranjal Patel <patelpranjal139@gmail.com>
2023-08-07 22:39:15 +0530

    Completed ECMAScript

--- 1_ES6script.js
+++ 1_ES6script.js
@@ -224,5 +224,80 @@
     return string.toLowerCase()
     }
 
-  */}
+    20.Reuse JavaScript Code Using import
+    import {uppercaseString, lowercaseString} from './string_functions.js';
+    // Only change code above this line
+    uppercaseString("hello");
+    lowercaseString("WORLD!");
+
+    21. Use * to Import Everything from a File
+    import * as stringFunctions from './string_functions.js'
+    // Only change code above this line
+
+    stringFunctions.uppercaseString("hello");
+    stringFunctions.lowercaseString("WORLD!");
+
+    22. Create an Export Fallback with export default
+    export default function subtract(x, y) {
+    return x - y;
+    }
+
+    23. Import a Default Export
+    import subtract from "./math_functions.js" 
+    // Only change code above this line
+    subtract(7,4);
+
+    24. Create a JavaScript Promise
+    const makeServerRequest = new Promise((resolve, reject)=>{
+    });
+    
+    25. Complete a Promise with resolve and reject
+    const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer represents a response from a server
+    let responseFromServer;
+    
+    if(responseFromServer) {
+        resolve("We got the data");
+    } else {  
+        reject("Data not received");
+    }
+    });
+
+    26. Handle a Fulfilled Promise with then
+    const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer is set to true to represent a successful response from a server
+    let responseFromServer = true;
+	
+    if(responseFromServer) {
+        resolve("We got the data");
+    } else {	
+        reject("Data not received");
+    }
+    });
+
+    makeServerRequest.then(result => {
+    console.log(result);
+    });
+
+    27.Handle a Rejected Promise with catch
+    const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer is set to false to represent an unsuccessful response from a server
+    let responseFromServer = false;
+    
+    if(responseFromServer) {
+        resolve("We got the data");
+    } else {  
+        reject("Data not received");
+    }
+    });
+
+    makeServerRequest.then(result => {
+    console.log(result);
+
+    makeServerRequest.catch(error => {
+    console.log(error);
+    })
+    });
+
+*/}
 </script>
